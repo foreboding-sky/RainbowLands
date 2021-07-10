78a8f9d5b516d8e2eba1b0d9327bc5ca230395dc
Nisharu <39903426+Nisharu@users.noreply.github.com>
2021-07-10 15:10:33 +0300

    - Tons of new enemies - Level manager singletion fix - Enemy script tweaks

--- VisualStudioC++_RainbowLands/RainbowLands/RainbowLands/Enemy.cpp
+++ VisualStudioC++_RainbowLands/RainbowLands/RainbowLands/Enemy.cpp
@@ -3,8 +3,9 @@
 using namespace godot;
 Enemy::Enemy() 
 {
-	_health = 3;
 	_speed = 100;
+	_health = 3;
+	_damage = 1;
 }
 
 Enemy::~Enemy()
@@ -16,16 +17,20 @@ void Enemy::_register_methods()
 	register_method((char*)"_physics_process", &Enemy::_physics_process);
 	register_method((char*)"_init", &Enemy::_init);
 	register_method((char*)"_ready", &Enemy::_ready);
-
+	register_property<Enemy, int>("Health", &Enemy::_health, 3);
+	register_property<Enemy, int>("Speed", &Enemy::_speed, 100);
+	register_property<Enemy, int>("Damage", &Enemy::_damage, 1);
 	register_method((char*)"_on_Area2D_area_entered", &Enemy::_on_Area2D_area_entered);
 }
 
 void Enemy::_init()
 {
+	_level_manager = LevelManager::get_singleton();
 }
 
 void Enemy::_ready()
 {
+
 }
 
 void Enemy::_physics_process(float delta)
@@ -33,7 +38,10 @@ void Enemy::_physics_process(float delta)
 	set_offset(get_offset() + _speed * delta);
 
 	if (get_unit_offset() >= 1)
-		queue_free();
+	{
+		_level_manager->_mob_got_through(_damage);
+			queue_free();
+	}
 }
 
 void Enemy::_on_Area2D_area_entered(Area2D* _other_area)
--- VisualStudioC++_RainbowLands/RainbowLands/RainbowLands/LevelManager.cpp
+++ VisualStudioC++_RainbowLands/RainbowLands/RainbowLands/LevelManager.cpp
@@ -2,15 +2,10 @@
 
 using namespace godot;
 
-LevelManager* LevelManager::_instance;
+LevelManager* LevelManager::_instance = nullptr;
 
-LevelManager::LevelManager()
-{
-}
-
-LevelManager::~LevelManager()
-{
-}
+LevelManager::LevelManager() {}
+LevelManager::~LevelManager() {}
 void LevelManager::Notify()
 {
 
@@ -31,14 +26,48 @@ void LevelManager::_register_methods()
 
 LevelManager* LevelManager::get_singleton()
 {
+	if (!_instance) _instance = new LevelManager();
 	return _instance;
-	//return nullptr;
 }
 
 void LevelManager::LoadEnemies()
 {
 	enemyRefs.push_back(_loader->load("res://TD/Enemies/Enemy.tscn"));
 	enemyThreat[enemyRefs[0]] = (int)1;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/Bug.tscn"));
+	enemyThreat[enemyRefs[1]] = (int)1;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/Ancient.tscn"));
+	enemyThreat[enemyRefs[2]] = (int)10;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/AncientThreat.tscn"));
+	enemyThreat[enemyRefs[3]] = (int)15;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/BugExtreme.tscn"));
+	enemyThreat[enemyRefs[4]] = (int)5;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/CrystalAngel.tscn"));
+	enemyThreat[enemyRefs[5]] = (int)4;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/DeathAngel.tscn"));
+	enemyThreat[enemyRefs[6]] = (int)8;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/Defender.tscn"));
+	enemyThreat[enemyRefs[7]] = (int)20;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/Goliath.tscn"));
+	enemyThreat[enemyRefs[8]] = (int)3;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/MagmaGoliath.tscn"));
+	enemyThreat[enemyRefs[9]] = (int)9;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/Moth.tscn"));
+	enemyThreat[enemyRefs[10]] = (int)35;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/Overlord.tscn"));
+	enemyThreat[enemyRefs[11]] = (int)30;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/OverlordExtreme.tscn"));
+	enemyThreat[enemyRefs[12]] = (int)100;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/PenBot.tscn"));
+	enemyThreat[enemyRefs[13]] = (int)25;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/Stalker.tscn"));
+	enemyThreat[enemyRefs[14]] = (int)2;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/Sword.tscn"));
+	enemyThreat[enemyRefs[15]] = (int)10;
+	enemyRefs.push_back(_loader->load("res://TD/Enemies/Wasp.tscn"));
+	enemyThreat[enemyRefs[16]] = (int)80;
+
+
 }
 
 void LevelManager::SpawnEnemy()
@@ -47,11 +76,11 @@ void LevelManager::SpawnEnemy()
 	{
 		int randomNumber = rand() % enemyRefs.size();
 		int i = enemyThreat[enemyRefs[randomNumber]];
-
+		spawnTimer->set_wait_time((float)i / 3.0);
 		if (i <= waveThreat)
 		{
 			waveThreat -= i;
-			spawnTimer->set_wait_time((float)i/3.0);
+;
 			enemy = cast_to<PathFollow2D>(enemyRefs[randomNumber]->instance());
 			get_node("/root/main/Path2D")->add_child(enemy);
 			break;
@@ -97,6 +126,7 @@ void LevelManager::_ready()
 	_max_health = 10;
 	_placement_currency = 10;
 	LoadEnemies();
+
 }
 
 void LevelManager::_level_over()
